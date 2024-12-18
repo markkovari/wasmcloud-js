b70fccd0f00b209a0d32fbfa51c5b7b06fb79d3f
Márk Kővári <kovarimarkofficial@gmail.com>
2024-12-18 13:03:33 +0100

    chore(wasm): import wasm to vite bundle
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter'
+import { HostKey } from "../wasmcloud-rs-js/pkg";
+
+const hostkey = new HostKey();
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <p>Pubic Key: ${hostkey.public_key}</p>
+    <p>Seed: ${hostkey.seed}</p>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
